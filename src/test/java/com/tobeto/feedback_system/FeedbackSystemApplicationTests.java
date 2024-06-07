770835d76549ce96c812279c9e247ce39e559ad1
ShmsErl <erol.sehmus1@gmail.com>
2024-06-07 08:40:18 +0300

    Created Feedback-System project and readme.md file.

---
+++ src/test/java/com/tobeto/feedback_system/FeedbackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto.feedback_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
