d2eee7c60ac3faacb5b40ea245acb03274d58ddf
ShmsErl <erol.sehmus1@gmail.com>
2024-06-07 09:52:12 +0300

    Added BaseEntity with common properties  Added BaseEntity class with id, createdDate, and lastModifiedDate  Implemented @PrePersist and @PreUpdate for automatic timestamps

---
+++ src/main/java/com/tobeto/feedback_system/models/abstracts/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.tobeto.feedback_system.models.abstracts;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+        lastModifiedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastModifiedDate = LocalDateTime.now();
+    }
+
+}
