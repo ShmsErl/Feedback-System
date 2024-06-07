a68984f79170f2299deb4de8a75ab57c71c6e3e1
ShmsErl <erol.sehmus1@gmail.com>
2024-06-07 10:03:10 +0300

    Added User entity with UserDetails implementation. Created User entity with id, username, password, enabled, and roles fields. Implemented UserDetails for Spring Security integration, including authorities and account status methods.

---
+++ src/main/java/com/tobeto/feedback_system/models/concretes/User.java
@@ -0,0 +1,76 @@
+package com.tobeto.feedback_system.models.concretes;
+
+import com.tobeto.feedback_system.models.abstracts.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User extends BaseEntity implements UserDetails {
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+
+    private boolean isEnabled;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    public User(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+        super(id, createdDate, lastModifiedDate);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.getRoleName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+}
